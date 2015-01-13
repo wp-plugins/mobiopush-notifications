c8626ac1a2f96b1d70fcdf1deb7bb8356777ba32
mobiopush <mobiopush@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 04:30:55 +0000

    new js code
    git-svn-id: https://plugins.svn.wordpress.org/mobiopush-notifications/trunk@1066638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mobiopush-plugin.php
+++ mobiopush-plugin.php
@@ -407,17 +407,11 @@ function add_checkbox() {
     function mp_footer() {
     
     ?>
+<!-- start of mobio code -->
+<script src="//cdn.mobiopush.com/mobiojs/<?php echo esc_attr( get_option('mobio_site_key') ); ?>" type="text/javascript" id="_mobio_js"></script>
+<!-- end of mobio code -->    
     
-    
-    <!-- start of mobio code -->
-<script type="text/javascript">
-	var mpush_site_key='<?php echo esc_attr( get_option('mobio_site_key') ); ?>'; 
-	var mpush_alias='default'; //optional
-	var mpush_segments='default'; //optional
-</script>
-<script src="http://cdn.mobiopush.com/mobio.js" type="text/javascript"></script>
-<!-- end of mobio code --> 
-    
+
   
     
     <?php
